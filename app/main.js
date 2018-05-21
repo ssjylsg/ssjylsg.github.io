bcb986226511a6e08aa4d6eb9f6db533f0f6aa1c
ssjylsg <ssjylsg@126.com>
2018-05-21 11:33:48 +0800

    format

--- app/main.js
+++ app/main.js
@@ -1,13 +1,11 @@
-require.config({　
-	　
+require.config({　　
 	paths: {　　　　　　
 		"jquery": "https://cdn.bootcss.com/jquery/3.3.1/jquery.min",
 		"map2d": "../map/map2d/js/Init",
-		"mapConfig":'../map/map2d/js/mapConfig.json',
-		 
+		"mapConfig": '../map/map2d/js/mapConfig.json',
 	},
-	shim:{
-		"dialog":{
+	shim: {
+		"dialog": {
 			deps: ['jquery'],
 			exports: 'dialog'
 		}
